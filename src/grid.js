2c7b9bdbaea149fd1332abf5ffd3bb9375d4fd32
gochinj <jeff@imginconline.com>
2015-05-08 16:39:11 -0400

    Fixing strange issue, with code disappearance

--- src/grid.js
+++ src/grid.js
@@ -2,8 +2,6 @@
 (function(angular, undefined){
     angular.module("noinfopath.ui")
 
-        //> noGrid currently use Kendo UI Grid. This will be changed in the future
-        //> to use an abstraction that will allow other grids to be swapped in.
         .directive("noGrid", ['$state','$q','lodash', 'noConfig', 'noManifest', 'noKendo', 'noIndexedDB', function($state, $q, _, noConfig, noManifest, noKendo, noIndexedDB){
             return {                
                 link: function(scope, el, attrs){
@@ -16,8 +14,6 @@
                         });
 
                     function _bindGrid(ds, config){
-                        if(!config.columns) throw "noGridConfig must have columns defined.";
-
                         var grid = {
                             groupable: config.groupable || false,
                             pageSize: config.pageSize || 10,
@@ -37,7 +33,7 @@
                                 }else{
                                     var tableName = this.dataSource.transport.tableName;
                                     scope.$root.$broadcast("noGrid::change+" + tableName, data);
-                                }
+                                }                            
                             }                              
                         };  
 
@@ -60,7 +56,6 @@
                     }
 
                     function _bindDS(tableName, config){
-                        if(!config.model) throw tableName + " noGridConfig must have a model defined.";
 
                         var ds = noKendo.makeKendoDataSource(tableName, noIndexedDB, {
                                 serverFiltering: true, 
@@ -124,22 +119,32 @@
                                 var area = attrs.noGridArea,
                                     tableName = attrs.noSharedDatasource,
                                     noTable = noManifest.current.indexedDB[tableName],
-                                    config = noConfig.current.noArea[area][tableName];
+                                    config = noConfig.current[area][tableName];
 
                                 _bindGrid(ds, config);                                      
 
                             });
-                        }else if($state.current.data && $state.current.data.noGrid){
-                            var config = $state.current.data.noGrid;
-
-                            _bindDS(config.tableName, config);
                         }else{
-                            var area = attrs.noGridArea,
-                                tableName = attrs.noGridDatasource,
-                                noTable = noManifest.current.indexedDB[tableName],
-                                config = noConfig.current[area][tableName];
+                            var config, noTable, noGrid,
+                                noArea = noConfig.current.noArea,
+                                areaName = attrs.noArea,
+                                routeName = attrs.noAreaRoute,
+                                viewName = attrs.noAreaView,
+                                tableName = attrs.noGridDatasource;
+                                
+                            if(!noArea) throw "noArea missing from config.json";
+
+                            if(viewName){
+                                config = noArea[areaName][routeName][viewName];
+                            }else{
+                                config = noArea[areaName][routeName];
+                            }
                                 
                             if(config){
+                                noGrid = config.data && config.data.noGrid ? config.data.noGrid : null;
+
+                                if(!noGrid) throw "noGrid configuration missing";
+
                                 if(config.values){
                                     _resolveLookups(config)
                                         .then(_bindDS.bind(this, tableName, config))
@@ -147,7 +152,7 @@
                                             console.log(err);
                                         });             
                                 }else{
-                                    _bindDS(tableName, config);
+                                    _bindDS(noGrid.tableName, noGrid);
                                 } 
                             }else{
                                 throw "noGrid configuration found.";
@@ -161,5 +166,4 @@
     var noInfoPath = {};
 
     window.noInfoPath = angular.extend(window.noInfoPath || {}, noInfoPath);
-
 })(angular);
