303e07f1d61ad8af07f942275fe1711b3b0681ce
IMG-iMAC-1 <img-imac-1@IMG-iMAC-1s-Mac-mini.local>
2016-03-22 10:18:04 -0400

    Fixed bug with data panel relating to recieving paged data

--- dist/noinfopath-ui.js
+++ dist/noinfopath-ui.js
@@ -1,7 +1,7 @@
 /*
  *  # noinfopath.ui
  *
- *  > @version 1.0.17
+ *  > @version 1.0.18
  *
 */
 
@@ -874,140 +874,142 @@
 })(angular);
 
 //data-panel.js
-(function(angular, undefined){
-    angular.module("noinfopath.ui")
-        /*
-        *   ##  noDataPanel
-        *
-        *   Renders a data bound panel that can contain
-        *   any kind of HTML content, which can be bound
-        *   data on $scope.  The data sources being bound
-        *   to, are NoInfoPath Data Providers. Note that
-        *   this directive calls noDataSource.one method,
-        *   only returns a single data object, not an array.
-        *
-        *   ### Sample Usage
-        *
-        *   This sample show how to use the noDataPanel
-        *   directive in your HTML markup.
-        *
-        *   ```html
-        *   <no-data-panel no-config="noForms.trialPlot.noComponents.selection"/>
-        *   ```
-        *
-        *   ### Sample Configuration
-        *
-        *   ```js
-        *   {
-        *       "selection": {
-        *           "scopeKey": "selection",
-        *           "dataProvider": "noWebSQL",
-        *           "databaseName": "FCFNv2",
-        *           "entityName": "vw_trialplot_selection",
-        *           "primaryKey": "TrialPlotID",
-        *           "lookup": {
-        *               "source": "$stateParams",
-        *           },
-        *           "templateUrl": "observations/selection.html"
-        *       }
-        *   }
-        *   ```
-        */
-        .directive("noDataPanel", ["$injector", "$q", "$http", "$compile", "noFormConfig", "noDataSource", "$state", function($injector, $q, $http, $compile, noFormConfig, noDataSource, $state){
+(function(angular, undefined) {
+  angular.module("noinfopath.ui")
+    /*
+     *   ##  noDataPanel
+     *
+     *   Renders a data bound panel that can contain
+     *   any kind of HTML content, which can be bound
+     *   data on $scope.  The data sources being bound
+     *   to, are NoInfoPath Data Providers. Note that
+     *   this directive calls noDataSource.one method,
+     *   only returns a single data object, not an array.
+     *
+     *   ### Sample Usage
+     *
+     *   This sample show how to use the noDataPanel
+     *   directive in your HTML markup.
+     *
+     *   ```html
+     *   <no-data-panel no-config="noForms.trialPlot.noComponents.selection"/>
+     *   ```
+     *
+     *   ### Sample Configuration
+     *
+     *   ```js
+     *   {
+     *       "selection": {
+     *           "scopeKey": "selection",
+     *           "dataProvider": "noWebSQL",
+     *           "databaseName": "FCFNv2",
+     *           "entityName": "vw_trialplot_selection",
+     *           "primaryKey": "TrialPlotID",
+     *           "lookup": {
+     *               "source": "$stateParams",
+     *           },
+     *           "templateUrl": "observations/selection.html"
+     *       }
+     *   }
+     *   ```
+     */
+    .directive("noDataPanel", ["$injector", "$q", "$http", "$compile", "noFormConfig", "noDataSource", "$state", function($injector, $q, $http, $compile, noFormConfig, noDataSource, $state) {
 
-            function _link(scope, el, attrs){
-                var config,
-                    resultType = "one",
-                    dataSource,
-                    noFormAttr = attrs.noForm;
+      function _link(scope, el, attrs) {
+        var config,
+          resultType = "one",
+          dataSource,
+          noFormAttr = attrs.noForm;
 
-                function finish(data){
-					if(data.paged){
-						scope[config.scopeKey] = data.paged[0];
-					} else {
-						scope[config.scopeKey] = data;
-					}
+        function finish(data) {
+          if (data.paged) {
+            scope[config.scopeKey] = data.paged;
+          } else {
+            scope[config.scopeKey] = data;
+          }
 
-                    if(config.hiddenFields){
-                        for(var h in config.hiddenFields){
-                            var hf = config.hiddenFields[h],
-                                value = noInfoPath.getItem(scope, hf.scopeKey);
+          if (config.hiddenFields) {
+            for (var h in config.hiddenFields) {
+              var hf = config.hiddenFields[h],
+                value = noInfoPath.getItem(scope, hf.scopeKey);
 
-                            //console.log(hidden);
-                            noInfoPath.setItem(scope, hf.ngModel, value);
-                        }
-                    }
+              //console.log(hidden);
+              noInfoPath.setItem(scope, hf.ngModel, value);
+            }
+          }
 
-                    if(scope.waitingFor ) {
-                        scope.waitingFor[config.scopeKey] = false;
-                    }
-
-
-                }
-
-                function error(err){
-                    scope.waitingForError = {error: err, src: config };
-
-                    console.error(scope.$root.waitingForError);
-                }
-
-                function noForm_ready(data){
-                    config = noInfoPath.getItem(data, noFormAttr);
-
-                    if(config.noDataPanel) {
-                        resultType = config.noDataPanel.resultType ? config.noDataPanel.resultType : "one";
-
-                        if(config.noDataPanel.refresh){
-                            scope.$watchCollection(config.noDataPanel.refresh.property, function(newval, oldval){
-                                if(newval){
-                                    dataSource[resultType]()
-                                        .then(finish)
-                                        .catch(error);
-                                }
-                            });
-                        }
-
-                    }
-
-                    if(config.noDataSource){
-                        dataSource = noDataSource.create(config.noDataSource, scope);
-                    }else{
-                        dataSource = noDataSource.create(config, scope);
-                    }
-
-                    if(config.templateUrl){
-                        $http.get(config.templateUrl)
-                            .then(function(resp){
-                                var t = $compile(resp.data),
-                                    params = [],
-                                    c = t(scope);
-
-                                el.append(c);
-
-                                dataSource[resultType]()
-                                    .then(finish)
-                                    .catch(error);
-                            });
-                    }else{
-                        dataSource[resultType]()
-                            .then(finish)
-                            .catch(error);
-                    }
-                }
+          if (scope.waitingFor) {
+            scope.waitingFor[config.scopeKey] = false;
+          }
 
 
-                noFormConfig.getFormByRoute($state.current.name, $state.params.entity, scope)
-                    .then(noForm_ready)
+        }
+
+        function error(err) {
+          scope.waitingForError = {
+            error: err,
+            src: config
+          };
+
+          console.error(scope.$root.waitingForError);
+        }
+
+        function noForm_ready(data) {
+          config = noInfoPath.getItem(data, noFormAttr);
+
+          if (config.noDataPanel) {
+            resultType = config.noDataPanel.resultType ? config.noDataPanel.resultType : "one";
+
+            if (config.noDataPanel.refresh) {
+              scope.$watchCollection(config.noDataPanel.refresh.property, function(newval, oldval) {
+                if (newval) {
+                  dataSource[resultType]()
+                    .then(finish)
                     .catch(error);
+                }
+              });
             }
 
-            return {
-                restrict: "E",
-                link: _link,
-                scope: false
-            };
-        }])
-    ;
+          }
+
+          if (config.noDataSource) {
+            dataSource = noDataSource.create(config.noDataSource, scope);
+          } else {
+            dataSource = noDataSource.create(config, scope);
+          }
+
+          if (config.templateUrl) {
+            $http.get(config.templateUrl)
+              .then(function(resp) {
+                var t = $compile(resp.data),
+                  params = [],
+                  c = t(scope);
+
+                el.append(c);
+
+                dataSource[resultType]()
+                  .then(finish)
+                  .catch(error);
+              });
+          } else {
+            dataSource[resultType]()
+              .then(finish)
+              .catch(error);
+          }
+        }
+
+
+        noFormConfig.getFormByRoute($state.current.name, $state.params.entity, scope)
+          .then(noForm_ready)
+          .catch(error);
+      }
+
+      return {
+        restrict: "E",
+        link: _link,
+        scope: false
+      };
+    }]);
 })(angular);
 
 //alpha-filter.js
