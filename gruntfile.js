39459d901018dc23f6f0321c3a51fcbfd8d1f35a
gochinj <jeff@imginconline.com>
2016-05-23 15:53:50 -0400

    Changed karma to use Chrome instead of PhantomJS.

--- gruntfile.js
+++ gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function(grunt) {
           continuous: {
             configFile: 'karma.conf.js',
             singleRun: true,
-            browsers: ['PhantomJS']
+            browsers: ['Chrome']
           }
         },
         bumpup: {
