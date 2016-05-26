d364735e222223f3a860896bafa2baa22e1eec5b
ryan <ryan@imginconline.com>
2016-05-26 15:20:33 -0400

    minor changes to noTabs for formatting

--- src/tabs.js
+++ src/tabs.js
@@ -4,18 +4,23 @@
 		.directive("noTabs", ["$compile", "$state", "noFormConfig", "noDataSource", function($compile, $state, noFormConfig, noDataSource) {
 			function _static(scope, el, attrs) {
 				console.log("static");
-				var ul = el.find("ul").first(),
+				var ul = el.find("ul")
+					.first(),
 					lis = ul.length > 0 ? ul.children() : null,
-					pnls = el.find("no-tab-panels").first().children("no-tab-panel"),
+					pnls = el.find("no-tab-panels")
+					.first()
+					.children("no-tab-panel"),
 					def = ul.find("li.active"),
 					defNdx;
 
-
 				pnls.addClass("ng-hide");
 
-				el.find("no-tab-panels").first().addClass("tab-panels");
+				el.find("no-tab-panels")
+					.first()
+					.addClass("tab-panels");
 
-				el.find("no-tab-panels > no-tab-panel > div").addClass("no-m-t-lg");
+				el.find("no-tab-panels > no-tab-panel > div")
+					.addClass("no-m-t-lg");
 
 				for (var lii = 0, ndx = 0; lii < lis.length; lii++) {
 					var lie = angular.element(lis[lii]);
@@ -25,33 +30,37 @@
 					}
 				}
 
-				lis.find("a:not(.filler-tab)").click(function(e) {
-					e.preventDefault();
+				lis.find("a:not(.filler-tab)")
+					.click(function(e) {
+						e.preventDefault();
 
 
-					var ul = el.find("ul").first(),
-						tab = ul.find("li.active"),
-						pnlNdx = Number(tab.attr("ndx")),
+						var ul = el.find("ul")
+							.first(),
+							tab = ul.find("li.active"),
+							pnlNdx = Number(tab.attr("ndx")),
+							pnl = angular.element(pnls[pnlNdx]);
+
+						tab.toggleClass("active");
+						pnl.toggleClass("ng-hide");
+
+						tab = angular.element(e.target)
+							.closest("li");
+						pnlNdx = Number(tab.attr("ndx"));
 						pnl = angular.element(pnls[pnlNdx]);
 
-					tab.toggleClass("active");
-					pnl.toggleClass("ng-hide");
+						tab.toggleClass("active");
+						pnl.toggleClass("ng-hide");
 
-					tab = angular.element(e.target).closest("li");
-					pnlNdx = Number(tab.attr("ndx"));
-					pnl = angular.element(pnls[pnlNdx]);
-
-					tab.toggleClass("active");
-					pnl.toggleClass("ng-hide");
-
-					scope.$broadcast("noTabs::Change", tab, pnl);
-				});
+						scope.$broadcast("noTabs::Change", tab, pnl);
+					});
 
 				//$compile(el.contents())(scope);
 
 				//Show defaul tab panel
 				defNdx = Number(def.attr("ndx"));
-				angular.element(pnls[defNdx]).toggleClass("ng-hide");
+				angular.element(pnls[defNdx])
+					.toggleClass("ng-hide");
 
 			}
 
@@ -71,50 +80,65 @@
 
 				ds.read()
 					.then(function(data) {
-						var ul = el.find("ul").first(),
-							pnls = el.find("no-tab-panels").first();
+						var ul = el.find("ul")
+							.first(),
+							pnls = el.find("no-tab-panels")
+							.first();
 
 						ul.addClass(_resolveOrientation(noTab.noTabs));
 
 						//pnls.addClass("ng-hide");
 
-						el.find("no-tab-panels").first().addClass("tab-panels");
+						el.find("no-tab-panels")
+							.first()
+							.addClass("tab-panels");
 
-						el.find("no-tab-panels > no-tab-panel > div").addClass("no-m-t-lg");
+						el.find("no-tab-panels")
+							.first()
+							.addClass("tab-panels col-sm-10");
+
+						el.find("no-tab-panels > no-tab-panel > div")
+							.addClass("no-m-t-lg");
 
 						for (var i = 0, ndx = 0; i < data.length; i++) {
 							var li = angular.element("<li></li>"),
+								a = angular.element("<a href=\"\#\"></a>"),
 								datum = data[i];
 							if (i === 0) {
 								li.addClass("active");
 							}
 							li.attr("ndx", datum[noTab.noTabs.valueField]);
-							li.text(datum[noTab.noTabs.textField]);
+							a.text(datum[noTab.noTabs.textField]);
+
+							li.append(a);
 
 							ul.append(li);
 						}
 						el.append(ul);
 
-						ul.find("li").click(function(e) {
-							e.preventDefault();
+						ul.find("li")
+							.click(function(e) {
+								e.preventDefault();
 
-							var ul = el.find("ul").first(),
-								tab = ul.find("li.active"),
-								pnlNdx = Number(tab.attr("ndx")),
+								var ul = el.find("ul")
+									.first(),
+									tab = ul.find("li.active"),
+									pnlNdx = Number(tab.attr("ndx")),
+									pnl = angular.element(pnls[pnlNdx]);
+
+								tab.toggleClass("active");
+								pnl.toggleClass("ng-hide");
+
+								tab = angular.element(e.target)
+									.closest("li");
+								pnlNdx = Number(tab.attr("ndx"));
 								pnl = angular.element(pnls[pnlNdx]);
 
-							tab.toggleClass("active");
-							pnl.toggleClass("ng-hide");
+								tab.toggleClass("active");
+								pnl.toggleClass("ng-hide");
 
-							tab = angular.element(e.target).closest("li");
-							pnlNdx = Number(tab.attr("ndx"));
-							pnl = angular.element(pnls[pnlNdx]);
-
-							tab.toggleClass("active");
-							pnl.toggleClass("ng-hide");
-
-							scope.$broadcast("noTabs::Change", tab, pnl);
-						});
+								scope.$broadcast("noTabs::Change", tab, pnl);
+							});
 					});
 			}
 
@@ -133,7 +157,7 @@
 				restrict: "E",
 				link: _link
 			};
-	}])
+		}])
 
 	;
 })(angular);
