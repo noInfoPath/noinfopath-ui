df490bb6ef00123514a02183064f3d28a9d07765
Mat <mat@imginconline.com>
2017-03-03 14:35:31 -0500

    Notification animates and has id

--- src/notify.js
+++ src/notify.js
@@ -1,81 +1,105 @@
 //notify.js
 (function(angular, undefined) {
-	"use strict";
-	function startup($rootScope) {
-		$(window.top.document.body).append("<no-notifications></no-notifications>");
-	}
+    "use strict";
 
-	function NoNotificationService($interval, $rootScope) {
-		var tpl = "<no-notification class=\"alert\" role=\"alert\"></no-notification>",
-			xtmp = "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>",
-			alertTypes = {
-				"warning": "alert-warning",
-				"info": "alert-info",
-				"danger": "alert-danger",
-				"success": "alert-success"
-			},
-			dismissible = "alert-dismissible",
-			timeout = 1000,
-			defaultOptions = {
-				ttl: 1, //in seconds
-				dismissible: false,
-				type: "info"
-			},
-			container = $("no-notifications");
+    function startup($rootScope) {
+        $(window.top.document.body).append("<no-notifications></no-notifications>");
+    }
 
-		function _appendMessage(message, options) {
-			var tmpOptions = angular.extend({}, defaultOptions, options),
-				notifcation = $(tpl),
-				btn = $(xtmp);
+    function NoNotificationService($interval, $rootScope) {
+		var ids = [];
 
-			notifcation.addClass(alertTypes[tmpOptions.type]);
+		var tpl = "<no-notification class=\"alert fadeIn\" role=\"alert\"></no-notification>",
+            xtmp = "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>",
+            alertTypes = {
+                "warning": "alert-warning",
+                "info": "alert-info",
+                "danger": "alert-danger",
+                "success": "alert-success"
+            },
+            dismissible = "alert-dismissible",
+            timeout = 1000,
+            defaultOptions = {
+                ttl: 1000, //in milliseconds
+                dismissible: false,
+                type: "info"
+            },
+            container = $("no-notifications");
 
-			if(tmpOptions.dismissible) {
-				notifcation.append(btn);
-				notifcation.attr("ttl", -1);
-				notifcation.attr("age", 0);
-				notifcation.addClass(dismissible);
+        function _appendMessage(message, options) {
+            var tmpOptions = angular.extend({}, defaultOptions, options),
+                notifcation = $(tpl),
+                btn = $(xtmp);
+
+			if(!tmpOptions.id) tmpOptions.id = noInfoPath.createUUID();
+
+			if(ids.indexOf(tmpOptions.id) > -1) {
+				return false;
 			} else {
-				notifcation.attr("ttl", tmpOptions.ttl);
-				notifcation.attr("age", 0);
+				ids.push(tmpOptions.id);
 			}
 
-			notifcation.append(message);
+            notifcation.addClass(alertTypes[tmpOptions.type]);
+			notifcation.attr("message-id", tmpOptions.id);
 
-			container.prepend(notifcation);
 
-			if(tmpOptions.dismissible) btn.click(removeMessage.bind(null, notifcation));
-		}
-		this.appendMessage = _appendMessage;
+            if (tmpOptions.dismissible) {
+                notifcation.append(btn);
+                notifcation.attr("ttl", -1);
+                notifcation.attr("age", 0);
+                notifcation.addClass(dismissible);
+            } else {
+                notifcation.attr("ttl", tmpOptions.ttl);
+                notifcation.attr("age", 0);
+            }
 
-		function removeMessage(el) {
-			el.remove();
-		}
+            notifcation.append(message);
 
-		function ageAndPurgeMessages() {
-			var messages = container.children();
+            container.prepend(notifcation);
 
-			for(var m=0; m < messages.length; m++) {
-				var el = $(messages[m]),
-					ttl = Number(el.attr("ttl")),
-					age = Number(el.attr("age"));
+            if (tmpOptions.dismissible) btn.click(removeMessage.bind(null, notifcation));
+        }
+        this.appendMessage = _appendMessage;
 
-				if(ttl > -1) {
-					if(age >= ttl) {
-						el.remove();
-					} else {
-						el.attr("age", age + 1);
-					}
-				}
-			}
-		}
+        function removeMessage(el) {
+			var messageId = el.attr("message-id"),
+				messageIdNdx = ids.indexOf(messageId);
 
-		$interval(ageAndPurgeMessages, timeout);
+			ids.splice(messageId, 1);
 
-	}
+			el.animate({
+				opacity: 0,
+				"padding-top": 0,
+				"padding-bottom": 0,
+				height: 0
+            }, '200', function() {
+                el.remove();
+            });
+        }
 
-	angular.module("noinfopath.ui")
-		.run(["$rootScope", startup])
-		.service("noNotificationService", ["$interval", "$rootScope", NoNotificationService])
-	;
+        function ageAndPurgeMessages() {
+            var messages = container.children();
+
+            for (var m = 0; m < messages.length; m++) {
+                var el = $(messages[m]),
+                    ttl = Number(el.attr("ttl")),
+                    age = Number(el.attr("age"));
+
+                if (ttl > -1) {
+                    if (age >= ttl) {
+                        removeMessage(el);
+                        break;
+                    } else {
+                        el.attr("age", age + 1000);
+                    }
+                }
+            }
+        }
+
+        $interval(ageAndPurgeMessages, timeout);
+    }
+
+    angular.module("noinfopath.ui")
+        .run(["$rootScope", startup])
+        .service("noNotificationService", ["$interval", "$rootScope", NoNotificationService]);
 })(angular);
