dcbefb05f62ee76a04b393cebebba70b169c0fe3
Adarian Eastman <adarian@imginconline.com>
2017-03-10 11:33:53 -0500

    documentation

--- src/data-panel.js
+++ src/data-panel.js
@@ -4,12 +4,10 @@
 
 	function NoDataPanelDirective($injector, $q, $compile, noFormConfig, noDataSource, noTemplateCache, $state, noParameterParser, PubSub, noAreaLoader) {
 		function version1(scope, el, attrs, ctx) {
+			var _scope;
+
 			function finish(data) {
-
-
 				if (resultType === "one") {
-
-
 					if (!!data && data.paged) {
 						noParameterParser.update(data.paged, _scope[config.scopeKey]);
 					} else if (!!data) {
@@ -131,49 +129,50 @@
 
 		}
 
-		function version2(scope, el, attrs, ctx) {	
-		    //NOTE: 1) noFormAttr not defined.
-			noAreaLoader.markComponentLoading($state.current.name, noFormAttr);
+		function version2(scope, el, attrs, noDataPanel, ctx) {
+			var defaultOptions = {
+      	resultType: "one"
+      },
+      _scope;	
+	    //NOTE: 1) noFormAttr not defined.
+			noAreaLoader.markComponentLoading($state.current.name, attrs.noForm);
 
-            //NOTE: 2) Use config.noDataPanel = angular.merge({}, defaultOptions, config.noDataPanel) instead of 
-            //      checking for the existence of `config.noDataPanel`.  So, create a variable called `defaultOptions`
-            //      and add default properties as needed.
-			if (config.noDataPanel && config.noDataPanel.saveOnRootScope) {
+      //NOTE: 2) Use config.noDataPanel = angular.merge({}, defaultOptions, config.noDataPanel) instead of 
+      //      checking for the existence of `config.noDataPanel`.  So, create a variable called `defaultOptions`
+      //      and add default properties as needed.
+      
+      noDataPanel = angular.merge({}, defaultOptions, noDataPanel);
+
+			if (noDataPanel.saveOnRootScope) {
 				_scope = scope.$root;
 			} else {
 				_scope = scope;
 			}
 
-            //NOTE: 3) As of V2 putting data source configuration directly on the config object is no longer
-            //      supported. So this `if` statement is not requried. Throw and exception if the the
-            //      `noDataSource` property is missing.
-			if (config.noDataSource) {
-				dataSource = noDataSource.create(config.noDataSource, scope, watch);
-			} else {
-				dataSource = noDataSource.create(config, scope, watch);
+      //NOTE: 3) As of V2 putting data source configuration directly on the config object is no longer
+      //      supported. So this `if` statement is not requried. Throw and exception if the the
+      //      `noDataSource` property is missing.
+			if (!config.noDataSource) {
+				throw "noDataPanel :: noDataSource is not defined";
 			}
 
-            //NOTE: 4) `scope` should be `_scope`
-			scope[ctx.component.scopeKey] = new noInfoPath.data.NoDataModel(dataSource);
+      //NOTE: 4) `scope` should be `_scope`
+			_scope[ctx.component.scopeKey] = new noInfoPath.data.NoDataModel(dataSource);
 
-            //NOTE: 5) Again, based on note #2 config.noDataPanel will always exists. Add `resultType: "one"`
-            //      to the `defaultOptions`. Remove the outer `if` statement and remove the assignment to
-            //      the `resultType`.
-			if (config.noDataPanel) {
-				resultType = config.noDataPanel.resultType ? config.noDataPanel.resultType : "one";
-
-				if (config.noDataPanel.refresh) {
-					scope.$watchCollection(config.noDataPanel.refresh.property, function (newval, oldval) {
-						if (newval) {
-							refresh();
-						}
-					});
-				}
+      //NOTE: 5) Again, based on note #2 config.noDataPanel will always exists. Add `resultType: "one"`
+      //      to the `defaultOptions`. Remove the outer `if` statement and remove the assignment to
+      //      the `resultType`.
+			if (noDataPanel.refresh) {
+				scope.$watchCollection(noDataPanel.refresh.property, function (newval, oldval) {
+					if (newval) {
+						refresh();
+					}
+				});
 			}			
 
-            //NOTE: 6) Move `templateUrl property to `noDataPanel` property.
-			if (config.templateUrl) {
-				noTemplateCache.get(config.templateUrl)
+      //NOTE: 6) Move `templateUrl property to `noDataPanel` property.
+			if (noDataPanel.templateUrl) {
+				noTemplateCache.get(noDataPanel.templateUrl)
 					.then(function (tpl) {
 						var t = $compile(tpl),
 							params = [],
@@ -188,18 +187,13 @@
 		}
 
 		function _link(scope, el, attrs) {
-			var config,  //NOTE: 7) Delete, ctx parameter will have what you need once you are in the Version functions.
-				resultType = "one",  //NOTE: 8) Delete; see note #5.
-				dataSource, //NOTE: 9) Delete; declare in the Version functions.
-				noFormAttr = attrs.noForm, //NOTE: 10) Delete; see note #1.
-				_scope, //NOTE: 11) Delete; declare in the Version functions.
-				ctx = noFormConfig.getComponentContextByRoute($state.current.name, $state.params.entity, scope),
+			var ctx = noFormConfig.getComponentContextByRoute($state.current.name, $state.params.entity, scope),
 				noDataPanel = angular.merge({}, {version: "1"}, ctx.component.noDataPanel);  //NOTE: 12a) pass this in to Version2 as a parameter.
 
 			if(noDataPanel.version === "1") {
 				version1(scope, el, attrs, ctx);
 			} else {
-				version2(scope, el, attrs, ctx);  //NOTE: 12b) New Prototype: Version2(scope, el, attrs, noDataPanel, ctx)
+				version2(scope, el, attrs, noDataPanel, ctx);  //NOTE: 12b) New Prototype: Version2(scope, el, attrs, noDataPanel, ctx)
 			}
 		}
 
@@ -218,9 +212,7 @@
 		 *   Renders a data bound panel that can contain
 		 *   any kind of HTML content, which can be bound
 		 *   data on $scope.  The data sources being bound
-		 *   to, are NoInfoPath Data Providers. Note that
-		 *   this directive calls noDataSource.one method,
-		 *   only returns a single data object, not an array.
+		 *   to are NoInfoPath Data Providers. 
 		 *
 		 *   ### Sample Usage
 		 *
@@ -228,7 +220,7 @@
 		 *   directive in your HTML markup.
 		 *
 		 *   ```html
-		 *   <no-data-panel no-config="noForms.trialPlot.noComponents.selection"/>
+		 *   <no-data-panel no-config="noForms.noComponents.selection"/>
 		 *   ```
 		 *
 		 *   ### Sample Configuration
@@ -249,34 +241,6 @@
 		 *   }
 		 *   ```
 		 */
-
-
-	// 	"cooperatorView": {
-	//       "scopeKey": "cooperatorView",
-	//       "noDataPanel": {
-	//         "refresh": {
-	//           "provider": "scope",
-	//           "property": "cooperator"
-	//         }
-	//       },
-	//       "noDataSource": {
-	//         "dataProvider": "noWebSQL",
-	//         "databaseName": "FCFNv2",
-	//         "entityName": "vw_cooperator_summary",
-	//         "primaryKey": "CooperatorID",
-	//         "filter": [
-	//           {
-	//             "field": "CooperatorID",
-	//             "operator": "eq",
-	//             "value": {
-	//               "source": "$stateParams",
-	//               "property": "id"
-	//             }
-	//           }
-	//         ]
-	//       }
-	//     }
-
 
 	.directive("noDataPanel", ["$injector", "$q", "$compile", "noFormConfig", "noDataSource", "noTemplateCache", "$state", "noParameterParser", "PubSub", "noAreaLoader", NoDataPanelDirective])
 	;
