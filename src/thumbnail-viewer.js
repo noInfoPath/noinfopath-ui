77727d59ab7709d5e81ca777d5a5300e79d66a2a
Mat <mat@imginconline.com>
2017-03-29 15:22:44 -0400

    fixed the flicker (again)

--- src/thumbnail-viewer.js
+++ src/thumbnail-viewer.js
@@ -320,10 +320,16 @@
 
 			if(!state.data.pristine) state.data.pristine = state.data.unsorted;
 
+
 			if (state.data.unsorted && !!state.data.unsorted.length) {
-				state.data.sorted = state.data.unsorted.sort(function (a, b) {
+				var sorted = state.data.unsorted.sort(function (a, b) {
 					return a[widget.orderByField] - b[widget.orderByField];
 				});
+
+                if(state.data.sorted[0]  && sorted[0][widget.fileIdField] === state.data.sorted[0][widget.fileIdField])
+                    return;
+                else
+                    state.data.sorted = sorted;
 
 				if (state.data.sorted[0] && (state.data.unsorted[0][widget.fileIdField] === state.data.sorted[0][widget.fileIdField])) {
 					_render(ctx, scope, state.element);
@@ -379,7 +385,7 @@
 			var pubSubID = PubSub.subscribe("noTabs::change", function (ctx, scope, tabInfo) {
 				var state = scope[ctx.componentType];
 
-				state.data.sorted = state.grid.dataSource.data().toJSON();
+				state.data.sorted = [];
 			}.bind(null, ctx, scope));
 
 
